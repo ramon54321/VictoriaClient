db72582065e3b30494951f1ae5543c3765af2774
Ramon Brand <ramon2009@gmail.com>
2018-01-09 14:48:50 +0200

    Reorganized solution. Added dlls correctly.

---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    VictoriaShared.Game.GameStateManager gm;
+
+	    SharpLogger.Logger.SetFilter("");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
